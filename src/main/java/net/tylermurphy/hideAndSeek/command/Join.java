86cbf25e0bc2b0550c6b28c1d51231880653f670
Tyler Murphy <tylerm@tylerm.dev>
2023-07-07 10:44:16 -0400

    update deps to 1.20

--- src/main/java/net/tylermurphy/hideAndSeek/command/Join.java
+++ src/main/java/net/tylermurphy/hideAndSeek/command/Join.java
@@ -2,13 +2,11 @@ package net.tylermurphy.hideAndSeek.command;
 
 import net.tylermurphy.hideAndSeek.Main;
 import net.tylermurphy.hideAndSeek.command.util.ICommand;
-import net.tylermurphy.hideAndSeek.configuration.Maps;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static net.tylermurphy.hideAndSeek.configuration.Config.errorPrefix;
 import static net.tylermurphy.hideAndSeek.configuration.Localization.message;
@@ -29,18 +27,6 @@ public class Join implements ICommand {
 			sender.sendMessage(errorPrefix + message("GAME_INGAME"));
 			return;
 		}
-		if(args.length > 0) {
-			if(Main.getInstance().getBoard().size() > 0) {
-				sender.sendMessage(errorPrefix + message("LOBBY_IN_USE"));
-				return;
-			}
-			net.tylermurphy.hideAndSeek.configuration.Map map = Maps.getMap(args[0]);
-			if(map == null) {
-				sender.sendMessage(errorPrefix + message("INVALID_MAP"));
-				return;
-			}
-			Main.getInstance().getGame().setCurrentMap(map);
-		}
 		Main.getInstance().getGame().join(player);
 	}
 
@@ -57,9 +43,6 @@ public class Join implements ICommand {
 	}
 
 	public List<String> autoComplete(@NotNull String parameter, @NotNull String typed) {
-		if(parameter.equals("*map")) {
-			return Maps.getAllMaps().stream().map(net.tylermurphy.hideAndSeek.configuration.Map::getName).collect(Collectors.toList());
-		}
 		return null;
 	}
 
